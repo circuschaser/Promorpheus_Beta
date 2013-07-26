837c868da327b9b6078fc08c37b73d4de328b189
circuschaser <rob@circuschaser.com>
2013-07-26 18:43:00 -0400

    manually deleted

--- db/migrate/20130724020947_song_tags_songs_join_table.rb
+++
@@ -1,8 +0,0 @@
-class SongTagsSongsJoinTable < ActiveRecord::Migration
-  def change
-  	create_table :song_tags_songs, id: false do |t|
-  		t.integer :song_id
-  		t.integer :song_tag_id
-  	end
-  end
-end
